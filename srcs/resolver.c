22d9eb1c32f462133397c0676bea79a9fe5fa779
Mateo Corso <mcorso@student.42.fr>
2022-03-21 16:42:47 +0100

    Implementing a decision maker part

---
+++ srcs/resolver.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   resolver.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 15:24:20 by mcorso            #+#    #+#             */
+/*   Updated: 2022/03/21 16:41:50 by mcorso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+static int	is_sorted(t_top stack_a)
+{
+	t_node	*curr_node;
+
+	curr_node = stack_a.top;
+	while (curr_node->val < curr_node->next->val)
+	{
+		curr_node = curr_node->next;
+		if (!curr_node)
+			return (0);
+	}
+	return (-1);
+}
+
+int	resolver(t_top *stack_a, t_top *stack_b, int argc)
+{
+	if (argc < 2)
+		return (0);
+	if (argc == 2)
+		if (stack_a->top->val < stack_a->top->next->val)
+			swap(stack_a, stack_b, 'a');
+	if (argc > 2)
+	{
+		if (is_sorted(*stack_a) == 0)
+			return (0);
+	}
+	return (0);
+}
