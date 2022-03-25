2cc63b71aa755f705c225fd24a446a78c78910fd
Mateo Corso <mcorso@student.42.fr>
2022-03-25 17:03:35 +0100

    Printing move only if a condition is met

--- srcs/move_manager.c
+++ srcs/move_manager.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 22:39:52 by mcorso            #+#    #+#             */
-/*   Updated: 2022/03/17 23:14:44 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/03/23 16:36:11 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,76 @@
 
 void	swap(t_top *stack_a, t_top *stack_b, char swap)
 {
-	if ((swap == 's' || swap == 'a') && stack_a->len > 2)
+	int	print;
+
+	print = 0;
+	if ((swap == 's' || swap == 'a') && stack_a->len >= 2)
+	{
 		logic_swap(stack_a);
-	if ((swap == 's' || swap == 'b') && stack_b->len > 2)
+		print = 1;
+	}
+	if ((swap == 's' || swap == 'b') && stack_b->len >= 2)
+	{
 		logic_swap(stack_b);
-	if (swap == 'a' || swap == 'b' || swap == 's')
+		print = 1;
+	}
+	if (print)
 		ft_printf("s%c\n", swap);
 }
 
 void	push(t_top *stack_a, t_top *stack_b, char swap)
 {
+	int	print;
+
+	print = 0;
 	if (swap == 'a' && stack_b->top)
+	{
 		logic_push(stack_a, stack_b);
+		print = 1;
+	}
 	if (swap == 'b' && stack_a->top)
+	{
 		logic_push(stack_b, stack_a);
-	if (swap == 'a' || swap == 'b')
+		print = 1;
+	}
+	if (print)
 		ft_printf("p%c\n", swap);
 }
 
 void	rotate(t_top *stack_a, t_top *stack_b, char swap)
 {
+	int	print;
+
+	print = 0;
 	if ((swap == 'a' || swap == 'r') && stack_a->len > 1)
+	{
 		logic_rotate(stack_a);
+		print = 1;
+	}
 	if ((swap == 'b' || swap == 'r') && stack_b->len > 1)
+	{
 		logic_rotate(stack_b);
-	if (swap == 'a' || swap == 'b' || swap == 'r')
+		print = 1;
+	}
+	if (print)
 		ft_printf("r%c\n", swap);
 }
 
 void	rev_rotate(t_top *stack_a, t_top *stack_b, char swap)
 {
+	int	print;
+
+	print = 0;
 	if ((swap == 'a' || swap == 'r') && stack_a->len > 1)
+	{
 		logic_reverse_rotate(stack_a);
+		print = 1;
+	}
 	if ((swap == 'b' || swap == 'r') && stack_b->len > 1)
+	{
 		logic_reverse_rotate(stack_b);
-	if (swap == 'a' || swap == 'b' || swap == 'r')
+		print = 1;
+	}
+	if (print)
 		ft_printf("rr%c\n", swap);
 }
