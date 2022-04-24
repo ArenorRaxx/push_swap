9c27f1eeeb583bd574b777323d3324d0dc708941
Mateo Corso <mcorso@student.42.fr>
2022-04-24 16:03:36 +0200

    Implementing basic bruteforce algorithme

--- srcs/resolver.c
+++ srcs/resolver.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 15:24:20 by mcorso            #+#    #+#             */
-/*   Updated: 2022/04/23 10:28:26 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/04/24 16:01:31 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,26 @@ static void	sort_three(t_top *stack_a)
 			rotate(stack_a, NULL, 'a');
 		}
 	}
+}
+
+int	brute_force(char **formated_args, t_top *stack_a, t_top *stack_b)
+{
+	char	*set;
+	t_top	tmp_a;
+
+	set = NULL;
+	if (init_stack(&tmp_a) < 0)
+		return (-1);
+	if (fill_stack(&tmp_a, formated_args) < 0)
+		return (-1);
+	while (is_sorted(tmp_a) < 0)
+	{
+		rev_pseudo_exec(set, tmp_a);
+		next_set(&set);
+		pseudo_exec(set, tmp_a);
+	}
+	exec(instruct, stack_a, stack_b);
+	return (0);
 }
 
 int	resolver(t_top *stack_a, t_top *stack_b, int argc)
